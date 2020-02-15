b92409874e928a3fdfc7be9f29e4a671c0ab30bc
Jacob Adams <jacob@tookmund.com>
2020-02-15 12:45:40 -0500

    remove unused code

--- auto.py
+++ auto.py
@@ -10,12 +10,6 @@ app.config["DEBUG"] = True
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
 
-# #for i in range(1):
-# 	pyautogui.moveTo(100, 100, duration=0.25)
-# 	pyautogui.moveTo(500, 100, duration=0.25)
-# 	pyautogui.moveTo(500, 500, duration=0.25)
-# 	pyautogui.moveTo(100, 500, duration=0.25)
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
 	if request.method == 'GET':
