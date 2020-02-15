a384357034386b24ab1998675637fb3002527519
Jacob Adams <jacob@tookmund.com>
2020-02-15 12:46:36 -0500

    Send accelerometer POST requests

--- static/script.js
+++ static/script.js
@@ -1,8 +1,33 @@
+var x = 0;
+var y = 0;
+var z = 0;
+var interval = 0;
+
 function onAccel(event) {
 	document.getElementById("x").innerHTML = event.acceleration.x;
 	document.getElementById("y").innerHTML = event.acceleration.y;
 	document.getElementById("z").innerHTML = event.acceleration.z;
 	document.getElementById("interval").innerHTML = event.interval;
+
+	x += event.acceleration.x*100;
+	y += event.acceleration.y*100;
+	z += event.acceleration.z*100;
+	interval += event.interval;
+
+	if (interval >= 0.5) {
+		sendMotion(x, y);
+		x = 0;
+		y = 0;
+		z = 0;
+		interval = 0;
+	}
+}
+
+function sendMotion(x, y) {
+    let xhttp = new XMLHttpRequest();
+	xhttp.open("POST", "/", true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send("x="+x+"&y="+y);
 }
 
 
